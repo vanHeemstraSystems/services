07d4917b264c957ac2274f05c123c104facb176c
Willem van Heemstra <willem@vanheemstrasystems.com>
2015-11-26 20:34:47 +0000

    Update test.sample.js

--- test.sample.js
+++ test.sample.js
@@ -12,11 +12,11 @@ console.log(server_prefix + " - Test service required.");
  * TEST READ
  *********************************************************************************************/
 exports.read = function(req, res) {
-	res.header("Access-Control-Allow-Origin", "*"); // to allow cross-domain, replace * with a list of domains is desired.
+    res.header("Access-Control-Allow-Origin", "*"); // to allow cross-domain, replace * with a list of domains is desired.
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'); // ExtJS will sent out OPTIONS
-	res.header('Content-Type', 'application/json');
+    res.header('Content-Type', 'application/json');
     res.send({ read: 'ok'});
 }
 
@@ -24,10 +24,10 @@ exports.read = function(req, res) {
  * TEST WRITE
  *********************************************************************************************/
 exports.write = function(req, res) {
-	res.header("Access-Control-Allow-Origin", "*"); // to allow cross-domain, replace * with a list of domains is desired.
+    res.header("Access-Control-Allow-Origin", "*"); // to allow cross-domain, replace * with a list of domains is desired.
     res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS'); // ExtJS will sent out OPTIONS
-	res.header('Content-Type', 'application/json');
+    res.header('Content-Type', 'application/json');
     res.send({ write: 'ok'});
 }
